d65429b7faef1ef488afc43cd98dbde2fe548d8b
Abhinav Srivastava <as@scoar.in>
2020-08-10 01:42:16 +0530

    Init Commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3001;
+
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  socket.on('changePage', (data) => socket.broadcast.emit('changePage', data));
+  socket.on('otherData', (data) => socket.broadcast.emit('otherData', data));
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
